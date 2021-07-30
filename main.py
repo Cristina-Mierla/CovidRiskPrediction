0f430a8d7299b041c014068244ec2aadd4725fe9
Mierlă Cristina <73819899+Cristina-ubb@users.noreply.github.com>
2021-07-30 12:15:37 +0300

    How does Flask work and how to GET and POST requests

--- main.py
+++ main.py
@@ -6,15 +6,74 @@ app = Flask(__name__)
 
 @app.route('/query-example')
 def query_example():
-    return 'Query String Example'
+    # if key doesn't exist, returns None
+    language = request.args.get('language')
 
-@app.route('/form-example')
+    # if key doesn't exist, returns a 400, bad request error
+    framework = request.args['framework']
+
+    # if key doesn't exist, returns None
+    website = request.args.get('website')
+
+    return '''
+                  The language value is: {}
+                  The framework value is: {}
+                  The website value is: {}'''.format(language, framework, website)
+
+@app.route('/form-example', methods=['GET', 'POST'])
 def form_example():
-    return 'Form Data Example'
+    if request.method == 'POST':
+        language = request.form.get('language')
+        framework = request.form.get('framework')
+        return '''
+                      <h1>The language value is: {}</h1>
+                      <h1>The framework value is: {}</h1>'''.format(language, framework)
 
-@app.route('/json-example')
+        # otherwise handle the GET request
+    return '''
+               <form method="POST">
+                   <div><label>Language: <input type="text" name="language"></label></div>
+                   <div><label>Framework: <input type="text" name="framework"></label></div>
+                   <input type="submit" value="Submit">
+               </form>'''
+
+@app.route('/json-example', methods=['POST'])
 def json_example():
-    return 'JSON Object Example'
+    request_data = request.json
+
+    language = None
+    framework = None
+    python_version = None
+    example = None
+    boolean_test = None
+
+    print(request_data)
+
+    if request_data:
+        if 'language' in request_data:
+            language = request_data['language']
+
+        if 'framework' in request_data:
+            framework = request_data['framework']
+
+        if 'version_info' in request_data:
+            if 'python' in request_data['version_info']:
+                python_version = request_data['version_info']['python']
+
+        if 'examples' in request_data:
+            if (type(request_data['examples']) == list) and (len(request_data['examples']) > 0):
+                example = request_data['examples'][0]
+
+        if 'boolean_test' in request_data:
+            boolean_test = request_data['boolean_test']
+
+
+    return '''
+                   The language value is: {}
+                   The framework value is: {}
+                   The Python version is: {}
+                   The item at index 0 in the example list is: {}
+                   The boolean value is: {}'''.format(language, framework, python_version, example, boolean_test)
 
 if __name__ == '__main__':
     # run app in debug mode on port 5000
